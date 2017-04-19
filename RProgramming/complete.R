7ce9c9ecc0002475d7a786c5f1d749d10fd6dc6a
MariaChowdhury <maria.chowdhury@gmail.com>
2017-04-19 00:53:12 -0700

    extra files deleted

--- RProgramming/complete.R
+++
@@ -1,9 +0,0 @@
-complete<-function(directory,id=1:332){
-  filenames<-list.files(directory,full.names=TRUE)
-  nobs<-numeric()
-  for(i in id ){
-    nobs<-c(nobs,sum(complete.cases(read.csv(filenames[i]))))
-  }
-  print(data.frame(id,nobs))
-  
-}
